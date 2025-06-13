a95bb915581addbb37b5f29199fad03bcb6cf47d
Flutter developer <fariha@devconst.com>
2025-06-13 11:38:26 +0500

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "31469eeab98bb87f09ddb2fcb0c86c0a",
+const RESOURCES = {"flutter_bootstrap.js": "a875efc6985b42090761061d4796fcc2",
 "version.json": "355e0812fd2eb825af090bbe1826daf1",
 "index.html": "f87145f99efe06f4f523bdf7ce4a8071",
 "/": "f87145f99efe06f4f523bdf7ce4a8071",
-"main.dart.js": "12dd918428c7bd8230e6147bd4b1c5b2",
+"main.dart.js": "a5ee1bfb65084c37dae6ac25d2f7a305",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "74e09818219e6f4ba8f2856be90d07d2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
